7a7c2eead96e4a60c0b5a8bb5756b0901169d812
SyahrulRamadhani29 <Ramabusiness2910@gmail.com>
2024-12-03 11:50:55 +0700

    add function tambahData

--- TugasBesar.java
+++ TugasBesar.java
@@ -1,6 +1,52 @@
 import java.util.Scanner;
 
 public class TugasBesar {
+    
+    static int MAX_MAHASISWA = 100;
+    static String[] nimMahasiswa = new String[MAX_MAHASISWA];
+    static String[] namaMahasiswa = new String[MAX_MAHASISWA];
+    static String[][] kodeMatkul = new String[MAX_MAHASISWA][10];
+    static String[][] namaMatkul = new String[MAX_MAHASISWA][10];
+    static int[][] sksMatkul = new int[MAX_MAHASISWA][10];
+    static int[] totalSks = new int[MAX_MAHASISWA];
+    static int[] jumlahMatkulPerMahasiswa = new int[MAX_MAHASISWA];
+    static int jumlahMahasiswa = 0;
+
+    static void tambahData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nama Mahasiswa: ");
+        String nama = sc.nextLine();
+        System.out.print("NIM: ");
+        String nim = sc.nextLine();
+
+        nimMahasiswa[jumlahMahasiswa] = nim;
+        namaMahasiswa[jumlahMahasiswa] = nama;
+
+        int jumlahMatkul = 0;
+        while (true) {
+            System.out.print("Kode Mata Kuliah: ");
+            String kode = sc.nextLine();
+            System.out.print("Nama Mata Kuliah: ");
+            String namaMk = sc.nextLine();
+            System.out.print("Jumlah SKS (1-3): ");
+            int sks = sc.nextInt();
+            sc.nextLine(); 
+
+            kodeMatkul[jumlahMahasiswa][jumlahMatkul] = kode;
+            namaMatkul[jumlahMahasiswa][jumlahMatkul] = namaMk;
+            sksMatkul[jumlahMahasiswa][jumlahMatkul] = sks;
+            totalSks[jumlahMahasiswa] += sks;
+
+            System.out.println("Data mata kuliah berhasil ditambahkan.");
+            System.out.print("Tambah mata kuliah lain? (y/t): ");
+            String lanjut = sc.nextLine();
+            if (lanjut.equalsIgnoreCase("t")) break;
+
+            jumlahMatkul++;
+        }
+
+        jumlahMatkulPerMahasiswa[jumlahMahasiswa] = jumlahMatkul + 1;
+        jumlahMahasiswa++;
     }
 
     static void tampilData() {
