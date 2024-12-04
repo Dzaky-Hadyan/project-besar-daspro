0af0cea45e26dfceda64845a78331b08d078fb3e
Dzaky-Hadyan <dzakyhadyaneliyanta06@gmail.com>
2024-12-04 07:05:39 +0700

    feat: change analisis data, add explanationary comment, add and erase leading space, refactor tambah data

--- TugasBesar.java
+++ TugasBesar.java
@@ -14,65 +14,66 @@ public class TugasBesar {
 
     static void tambahData() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n--- Tambah Data KRS ---");
         System.out.print("Nama Mahasiswa: ");
-        String nama = sc.nextLine();
+        namaMahasiswa[jumlahMahasiswa] = sc.nextLine();
         System.out.print("NIM: ");
-        String nim = sc.nextLine();
-
-        nimMahasiswa[jumlahMahasiswa] = nim;
-        namaMahasiswa[jumlahMahasiswa] = nama;
+        nimMahasiswa[jumlahMahasiswa] = sc.nextLine();
 
         int jumlahMatkul = 0;
         while (true) {
             System.out.print("Kode Mata Kuliah: ");
-            String kode = sc.nextLine();
+            kodeMatkul[jumlahMahasiswa][jumlahMatkul] = sc.nextLine();
             System.out.print("Nama Mata Kuliah: ");
-            String namaMk = sc.nextLine();
+            namaMatkul[jumlahMahasiswa][jumlahMatkul] = sc.nextLine();
             System.out.print("Jumlah SKS (1-3): ");
-            int sks = sc.nextInt();
+            sksMatkul[jumlahMahasiswa][jumlahMatkul] = sc.nextInt();
             sc.nextLine();
-            if (sks > 3) {
+            if (sksMatkul[jumlahMahasiswa][jumlahMatkul] > 3) {
                 System.out.println("Jumlah SKS tidak valid! SKS harus antara 1 dan 3.");
                 continue;
             } else {
-            kodeMatkul[jumlahMahasiswa][jumlahMatkul] = kode;
-            namaMatkul[jumlahMahasiswa][jumlahMatkul] = namaMk;
-            sksMatkul[jumlahMahasiswa][jumlahMatkul] = sks;
-            totalSks[jumlahMahasiswa] += sks;
-
+            totalSks[jumlahMahasiswa] += sksMatkul[jumlahMahasiswa][jumlahMatkul];
+            /*totalSks[0] = 0, sksMatkul[0][0] = 2
+             * totalSks[0] = 0 + 2 = 2, jumlahMatkul++
+             * sksMatkul[0][1] = 3
+             * totalSks[0] = 2 + 3 = 5
+             */
             System.out.println("Data mata kuliah berhasil ditambahkan.");
             System.out.print("Tambah mata kuliah lain? (y/t): ");
             String lanjut = sc.nextLine();
-            if (lanjut.equalsIgnoreCase("t")) break;
-
+            if (lanjut.equalsIgnoreCase("t")) {
+                System.out.printf("Total SKS yang diambil: %d \n", totalSks[jumlahMahasiswa]);
+                break;
+            }
             jumlahMatkul++;
             }
         }
-        jumlahMatkulPerMahasiswa[jumlahMahasiswa] = jumlahMatkul + 1;
+        jumlahMatkulPerMahasiswa[jumlahMahasiswa] = jumlahMatkul + 1; //jumlahMatkulPerMahasiswa[0] = 2
         jumlahMahasiswa++;
     }
-
     static void tampilData() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n--- Tampilkan Daftar KRS Mahasiswa ---");
         System.out.print("Masukkan NIM mahasiswa: ");
         String nim = sc.nextLine();
-
-        System.out.println("Daftar KRS: ");
+        System.out.println("\nDaftar KRS: ");
         System.out.printf("%-15s %-25s %-20s %-20s %-25s%n", "NIM", "Nama", "Kode MK", "Nama Mata Kuliah", "SKS");
         for (int i = 0; i < jumlahMahasiswa; i++) {
+            if (i == jumlahMahasiswa - 1 && !nimMahasiswa[i].equals(nim)) {
+                System.out.println("NIM yang anda cari tidak ada\n");
+                continue;
+            }
             if (nimMahasiswa[i].equals(nim)) {
                 for (int j = 0; j < jumlahMatkulPerMahasiswa[i]; j++) {
                     System.out.printf("%-15s %-25s %-20s %-20s %-25s%n", nimMahasiswa[i], namaMahasiswa[i], kodeMatkul[i][j], namaMatkul[i][j], sksMatkul[i][j]);
                 }
-                if (totalSks[i] < 20) {
-                    jumlahMahasiswaSKSKurangdari20++;
-                }
-                System.out.printf("Total SKS: %d\n", totalSks[i]);
+                System.out.printf("Total SKS: %d \n", totalSks[i]);
             }
         }
     }
+    static boolean[] sudahDihitung = new boolean[MAX_MAHASISWA];
     static void analisisData() {
-        boolean[] sudahDihitung = new boolean[jumlahMahasiswa];
         for (int i = 0; i < jumlahMahasiswa; i++) {
             if (!sudahDihitung[i]) {
                 int totalSKSperMahasiswa = 0;
@@ -88,12 +89,21 @@ public class TugasBesar {
                 }
             }
         }
-        /* emu 5, nene 3, rui 2
-         * 
-         * 
+        /*sudahDihitung[0], !sudahDihitung[0](belum dihitung)
+         * nim = nimMahasiswa[0] = 21345689
+         * nimMahasiswa[0].equals(21345689)
+         * totalSKSperMahasiswa = totalSks[0] = 5, sudahDihitung[0] = true, j++
+         * !nimMahasiswa[1].equals(21345689)
+         * totalSKSperMahasiswa < 20, jumlahMahasiswaSKSKurangdari20 = 1, i++
+         * !sudahDihitung[1]
+         * nim = nimMahasiswa[1] = 22014588
+         * !nimMahasiswa[0].equals(22014588), j++
+         * nimMahasiswa[1].equals(22014588)
+         * totalSKSperMahasiswa = totalSks[1] = 3, sudahDihitung[1] = true
+         * totalSKSperMahasiswa < 20, jumlahMahasiswaSKSKurangdari20 = 2, i++
          */
-        System.out.println("--- Analisis Data KRS ---");
-        System.out.printf("Jumlah mahasiswa yang mengambil SKS kurang dari 20: %d\n", jumlahMahasiswaSKSKurangdari20);
+        System.out.println("\n--- Analisis Data KRS ---");
+        System.out.printf("Jumlah mahasiswa yang mengambil SKS kurang dari 20: %d \n", jumlahMahasiswaSKSKurangdari20);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -123,6 +133,7 @@ public class TugasBesar {
                 default:
                     System.out.println("Pilihan tidak valid.");
             }
+            System.out.println();
         } while (menu != 4);
         sc.close();
     }
